3bd409e8bc5425de82be02958e7cb2aeb9ba9137
Shingugitvr000 <Shingugitvr000@gmail.com>
2025-08-19 10:03:10 +0900

    [Feat] 데코레이터 패턴 설정

--- Assets/Scripts/3_State/IdleState.cs
+++ Assets/Scripts/3_State/IdleState.cs
@@ -6,8 +6,9 @@ public class IdleState : IPlayerState
     public void HandleInput(SimplePlayer player)
     {
         float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
 
-        if (Mathf.Abs(horizontal) > 0.1f)
+        if (Mathf.Abs(horizontal) > 0.1f || Mathf.Abs(vertical) > 0.1f)
         {
             player.ChangeState(player.GetWalkState());
         }
--- Assets/Scripts/3_State/WalkState.cs
+++ Assets/Scripts/3_State/WalkState.cs
@@ -6,10 +6,14 @@ public class WalkState : IPlayerState
     public void HandleInput(SimplePlayer player)
     {
         float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
 
-        if (Mathf.Abs(horizontal) > 0.1f)
+        if (Mathf.Abs(horizontal) > 0.1f || Mathf.Abs(vertical) > 0.1f)
         {
+            // 수평 이동
             player.transform.Translate(Vector3.right * horizontal * 3f * Time.deltaTime);
+            // 수직 이동
+            player.transform.Translate(Vector3.up * vertical * 3f * Time.deltaTime);
         }
         else
         {
